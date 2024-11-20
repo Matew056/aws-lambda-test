6bfa6e363ef426d679dbe7fd59c9f3257e3931d3
Matew056 <mateodemian@gmail.com>
2024-11-20 00:20:02 -0300

    AddTask2attempt

--- src/addTask.js
+++ src/addTask.js
@@ -1,4 +1,29 @@
+const { v4 } = require('uuid');
+
 const addTask = async(event) => {
+
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+    const { title, description } = JSON.parse(event.body);
+    const createdAt = new Date()
+    const id = v4()
+
+    const newTask = {
+        id,
+        title,
+        description,
+        createdAt,
+    }
+
+    await dynamoDB.put({
+        TableName: 'TaskTable'
+        Item: newTask
+    }).promise()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(newTask)
+    }
 
 }
 
