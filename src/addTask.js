8f09067153d9542788b25c8144d2d98c1d18f8fd
Matew056 <mateodemian@gmail.com>
2024-12-20 05:48:12 -0300

    getTasks working, comments on addTask

--- src/addTask.js
+++ src/addTask.js
@@ -1,15 +1,19 @@
+// importar uuid para generar ids unicas
 const { v4 } = require('uuid');
-
+// importar AWS
 const AWS = require('aws-sdk');
 
+// funcion principal
 const addTask = async(event) => {
-
+    // asignar dynamodb
     const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
+    // generar atributos de la tabla
     const { title, description } = JSON.parse(event.body);
     const createdAt = new Date()
     const id = v4()
 
+    // crear la tabla con atributos
     const newTask = {
         id,
         title,
@@ -17,17 +21,20 @@ const addTask = async(event) => {
         createdAt
     }
 
+    // enviar datos
     await dynamoDB.put({
         TableName: 'TaskTable',
         Item: newTask
     }).promise()
 
+    // convertir a json newtask
     return {
-        statusCode: 200,
+        status: 200,
         body: JSON.stringify(newTask)
     }
 }
 
+// fin 
 module.exports = {
     addTask,
-} 
+}
---
+++ src/getTasks.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+
+    const getTasks = async (event) => {
+            console.log('initializing function')
+
+            const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+            console.log('dynamodb loaded')
+
+            const result = await dynamodb.scan({
+                TableName: 'TaskTable'
+            }).promise()
+
+            console.log('result loaded')
+
+            const tasks = result.Items
+
+            console.log('const tasks loaded')
+
+            return {
+                status: 200,
+                body: JSON.stringify(tasks)
+                    
+            }
+    }
+
+console.log('about to export')
+
+module.exports = {
+    getTasks
+}
